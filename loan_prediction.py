8205682bb74f8ec6ff9e63d37bbefb22bfa07112
Prasanna <jhaprasanna2006@gmail.com>
2024-10-26 10:52:59 +0530

    Changed loan_prediction.py

--- loan_prediction.py
+++ loan_prediction.py
@@ -3,7 +3,6 @@ import sklearn
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsRegressor
